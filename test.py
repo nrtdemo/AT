14a24c7c8b66a44a9619a2eaa73be2b0d3d8c5ff
nrtdemo <nrtdemo@hotmail.com>
2018-03-14 02:53:15 -0400

    for test only

--- test.py
+++ test.py
@@ -9,7 +9,16 @@ t = template_AT()
 
 
 t.print_header()
-t.print_menu()
-print '<input type="text" id="X45" idfill="9" for="X45" dvdvar="" scripttype="comfill" tabindex="" buttonid="" datachangeevent="" value="กสท คลองหลวง_คลองหลวง" flag="005" style="height:18px;margin-top:1px" name="instance/oss.source" maxlength="" onfocus="hpsm.widgets.Combo.handleOnFocus(this, event);" onblur="hpsm.widgets.Combo.handleOnBlur(this, event);" onclick="handleOnClick(this, event);" onchange="handleOnChange(this, event);" onkeydown="" aria-required="true">'
-t.print_close()
+# t.print_menu()
+print '''<input type="text" id="X45" idfill="9" for="X45" dvdvar="" scripttype="comfill" tabindex="" buttonid="" datachangeevent="" value="สค.หาดใหญ่ (โครงการ IP Core 100G Over DWDM)_หาดใหญ่" flag="005" style="height:18px;margin-top:1px" name="instance/oss.source" maxlength="" onfocus="hpsm.widgets.Combo.handleOnFocus(this, event);" onblur="hpsm.widgets.Combo.handleOnBlur(this, event);" onclick="handleOnClick(this, event);" onchange="handleOnChange(this, event);" onkeydown="" aria-required="true">'''
+print '''<input type="text" id="X49" name="instance/oss.destination" dvdvar="" buttonid="" datatype="string" sctype="Text" tabindex="" style="height:18px;margin-top:1px;" maxlength="" onkeyup="
+                  handleOnChange(this, event);
+                " onfocus="handleOnFocus(this, event);" onblur="handleOnBlur(this, event); applyToSameControl(this);" onclick="handleOnClick(this, event);" onchange="handleOnChange(this, event);" value="Teleport ศรีราชา (โครงการ IP Core 100G Over DWDM)_ศรีราชา" scripttype="text">'''
+print '''<textarea id="X53" rows="2" cols="30" buttonid="" dvdvar="instance/oss.address/oss.address" tabindex="" sctype="MultiText" style="height:65px;" name="instance/oss.address/oss.address" onfocus="handleOnFocus(this, event);" onblur="handleOnBlur(this, event); applyToSameControl(this);" onclick="handleOnClick(this, event);" onchange="handleOnChange(this, event);" maxlength="" onkeyup="
+    lockFormOnValueChange(this);
+  ">เลขที่  หมู่  สค.หาดใหญ่ (ห้อง IP core Network)  ซอย  ชั้น 4 ถนน  ตำบล/แขวง บางกระสอ อำเภอ/เขต หาดใหญ่ จังหวัด สงขลา </textarea>'''
+print '''<input type="text" id="X34" name="instance/oss.bandwidth" dvdvar="" buttonid="" datatype="string" sctype="Text" tabindex="" style="height:18px;margin-top:1px;" maxlength="" onkeyup="
+                  handleOnChange(this, event);
+                " onfocus="handleOnFocus(this, event);" onblur="handleOnBlur(this, event); applyToSameControl(this);" onclick="handleOnClick(this, event);" onchange="handleOnChange(this, event);" value="100 Gbps" scripttype="text">'''
+# t.print_close()
 
