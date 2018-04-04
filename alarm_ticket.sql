297447c685c5e22e4015f4e4d5331e914e380fd1
root <root@localhost.localdomain>
2018-04-04 00:38:45 -0400

    updata database

--- alarm_ticket.sql
+++ alarm_ticket.sql
@@ -16,30 +16,32 @@ CREATE TABLE splunk
   COLLATE = utf8mb4_unicode_ci;
 
 -- auto-generated definition
-CREATE TABLE tts
+create table tts
 (
-  ticketNo        VARCHAR(50)  NOT NULL
-    PRIMARY KEY,
-  incident_id     VARCHAR(255) NOT NULL,
-  affected_item   VARCHAR(255) NOT NULL,
-  cat_id          VARCHAR(255) NOT NULL,
-  status          VARCHAR(255) NOT NULL,
-  problem_status  VARCHAR(255) NOT NULL,
-  downtime_start  VARCHAR(255) NOT NULL,
-  downtime_time   VARCHAR(255) NOT NULL,
-  owner_group     VARCHAR(255) NOT NULL,
-  repairteam      VARCHAR(255) NOT NULL,
-  oss_source      VARCHAR(255) NULL,
-  oss_destination VARCHAR(255) NULL,
-  address         TEXT         NULL,
-  title           VARCHAR(255) NULL,
-  description     TEXT         NULL,
-  activity        MEDIUMTEXT   NULL,
-  CONSTRAINT tts_ticketNo_uindex
-  UNIQUE (ticketNo)
+  ticketNo        varchar(50)  not null
+    primary key,
+  incident_id     varchar(255) not null,
+  affected_item   varchar(255) not null,
+  cat_id          varchar(255) not null,
+  status          varchar(255) not null,
+  problem_status  varchar(255) not null,
+  downtime_start  varchar(255) not null,
+  downtime_time   varchar(255) not null,
+  owner_group     varchar(255) not null,
+  repairteam      varchar(255) not null,
+  oss_source      varchar(255) null,
+  oss_destination varchar(255) null,
+  bandwidth       varchar(255) null,
+  address         text         null,
+  title           varchar(255) null,
+  description     text         null,
+  activity        mediumtext   null,
+  constraint tts_ticketNo_uindex
+  unique (ticketNo)
 )
-  ENGINE = InnoDB
-  COLLATE = utf8mb4_unicode_ci;
+  engine = InnoDB
+  collate = utf8mb4_unicode_ci;
+
 
 -- auto-generated definition
 create table online_active
