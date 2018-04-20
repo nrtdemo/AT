3e340ebb4754fd2096b29dabc141b71e6039ab13
Nrtdemo <nrtdemo@hotmail.com>
2018-04-20 07:45:01 -0400

    update  requirement package

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='Alarmticket_AT',
+    version='1.0',
+    packages=['link', 'models', 'splunksdk.tests', 'splunksdk.tests.modularinput', 'splunksdk.tests.searchcommands',
+              'splunksdk.utils', 'splunksdk.examples.analytics', 'splunksdk.splunklib',
+              'splunksdk.splunklib.modularinput', 'splunksdk.splunklib.searchcommands', 'src_script'],
+    url='',
+    license='',
+    author='Nrtdemo',
+    author_email='',
+    description=''
+)
